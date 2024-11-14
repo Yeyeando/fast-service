196dc42a5e69710fb297f81b74bf3fcee61789ee
Yeyeando <sercacsal2@gmail.com>
2024-11-14 20:41:04 +0000

    arreglando header

--- fast-service/src/components/Gallery/Gallery.tsx
+++ fast-service/src/components/Gallery/Gallery.tsx
@@ -7,7 +7,7 @@ const ImageGallery: React.FC = () => {
     <div className="container-fluid">
       <div className="row g-5">
         {imagesData.images.map((image) => (
-          <div className="col-sm-3" key={image.id}>
+          <div className="col-6" key={image.id}>
             <div className="image-container">
               <img src={image.url} alt={image.alt} className="img-fluid" />
               <div className="overlay">
